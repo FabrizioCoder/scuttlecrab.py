7ed7170555afd2d7a885ffa9bc308b2bc38cb112
FabrizioCoder <fabrizioivan36@gmail.com>
2022-07-14 16:29:01 -0500

    true?

--- luxanna/main.py
+++ luxanna/main.py
@@ -1,3 +1,26 @@
+import disnake
 from luxanna.classes.bot import CustomBot
 
 bot = CustomBot()
+
+
+@bot.event
+async def on_message(message: disnake.Message) -> None:
+    """
+    The code in this event is executed every time someone sends a message, with or without the prefix
+    :param message: The message that was sent.
+    """
+
+    if message.author == bot.user or message.author.bot:
+        return
+
+    if message.guild.me.guild_permissions.manage_messages == False:
+        try:
+            message.author.send(
+                content=f"❌ Missing `Send Messages` permission in `{message.channel.name}`"
+            )
+        except:
+            return
+        return
+
+    await bot.process_commands(message)
